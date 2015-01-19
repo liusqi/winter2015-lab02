cd7b9de9cb92d586779f143fb3e275516a9e1f0b
Songqi Liu <liusqi1992@gmail.com>
2015-01-18 16:14:10 -0800

    image worked. Can't get the navigation work Signed-off-by:Songqi Liu <liusqi1992@gmail.com>

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -15,8 +15,8 @@
                 <span class="mynav">
                     <ul>
                         <li><a href="/">Home</a></li>
-                        <li><a href="/gallery.php">Gallery</a></li>
-                        <li><a href="/about.php">About</a></li>
+                        <li><a href="/gallery">Gallery</a></li>
+                        <li><a href="/about">About</a></li>
                     </ul>
                 </span>
             </div>
@@ -25,13 +25,13 @@
                 <table cols="3" class="gallery">
                     <tr>
                         <td class="oneimage">
-                            <a href="../data/Katmai_Crater_1980.jpg" data-lightbox="gallery" data-title="Katmai Crater - Mount Katmai, Alaska ... Posted 2014.05.05 by donald, in landscape"><img src="./data/thumb/Katmai_Crater_1980.jpg"/></a>
+                            <a href="./data/Katmai_Crater_1980.jpg" data-lightbox="gallery" data-title="Katmai Crater - Mount Katmai, Alaska ... Posted 2014.05.05 by donald, in landscape"><img src="./data/thumb/Katmai_Crater_1980.jpg"/></a>
                         </td>
                         <td class="oneimage">
-                            <a href="../data/Caledonian_orogeny_fold_in_King_Oscar_Fjord.jpg" data-lightbox="gallery" data-title="Caledonian orogeny fold in King Oscar Fjord ... Posted 2014.05.04 by mickey, in landscape"><img src="./data/thumb/Caledonian_orogeny_fold_in_King_Oscar_Fjord.jpg"/></a>
+                            <a href="./data/Caledonian_orogeny_fold_in_King_Oscar_Fjord.jpg" data-lightbox="gallery" data-title="Caledonian orogeny fold in King Oscar Fjord ... Posted 2014.05.04 by mickey, in landscape"><img src="./data/thumb/Caledonian_orogeny_fold_in_King_Oscar_Fjord.jpg"/></a>
                         </td>
                         <td class="oneimage">
-                            <a href="../data/Lands_End_Cape_Cornwall.jpg" data-lightbox="gallery" data-title="Land's End, Cape Cornwall and the Brisons ... Posted 2014.05.04 by mickey, in landscape"><img src="./data/thumb/Lands_End_Cape_Cornwall.jpg"/></a>
+                            <a href="./data/Lands_End_Cape_Cornwall.jpg" data-lightbox="gallery" data-title="Land's End, Cape Cornwall and the Brisons ... Posted 2014.05.04 by mickey, in landscape"><img src="./data/thumb/Lands_End_Cape_Cornwall.jpg"/></a>
                         </td>
                     </tr>
                 </table>
